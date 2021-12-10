3cda57f9836ae515f7f0206240f3010bb6311089
DayLaFavor <danelafavor1@gmail.com>
2021-12-10 15:02:31 -0600

    touch ups

--- src/App.js
+++ src/App.js
@@ -3,27 +3,15 @@ import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
 import styled from 'styled-components';
-import Details from './components/Details';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [star, setStar] = useState();
-  const [buttonOpen, setButtonOpen]= useState()
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
     
-  const openDetails = id => {
-    const character = star.filter(item => item.id === id);
-    setButtonOpen(character);
-  };
-  const closeDetails = () => {
-    setButtonOpen();
-  };
-
-
-
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people`)
       .then(res => {
@@ -31,43 +19,30 @@ const App = () => {
         setStar(res.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  });
 
  
   
-  const StarDiv = styled.div`
-  margin: 0 auto;
+  const divStyle = styled.div`
+  margin: auto;
   box-sizing: border-box;
-  width: 50%;
-  display:flex;
-  flex-direction:column;
-  justify-content:space-between;
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin-bottom:10%;
-  button {
-    width: 30%;
-    margin-left:35%;
-  }
-  
 `
- 
+
   return (
     <div className="App">
     <h1 className="Header">Characters</h1>
-    <StarDiv>
+    <divStyle>
     {
-       star && star.map(info =>
-       {
-           return <Character key={info.name}
-               info={info}
-               openDetails={openDetails}
-               closeDetails={closeDetails} />;
+      star && star.map(info => {
+        return <Character key={info.name}
+        info={info}/>;
        })
    }
-   {
-       buttonOpen && <Details info={buttonOpen} closeDetails={closeDetails} />
-    }
-    </StarDiv>
+    </divStyle>
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,48 +1,25 @@
 // Write your Character component here
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const Character = styled.div`
+const Style = styled.div`
 display: flex;
-flex-direction: column;
-width: 60%;
-align-items: center;
-border: 2px dashed black; 
-margin: 5%;
-padding: 1%;
-color: 'purple'
-opacity: 0.4;
-transform: scale(1.1); 
-animation: ${kf} 0.1s ease-in-out forwards;
+justify-content: center;
+margin:15% 35% 0;
+background-color: #B8860B;
+border: solid black 5px;
+border-radius: 10%;
+padding: 2%;
 `
-
-export default function Character({items}) {
-
-    return(
-        <div className='container'>
-         {items.map((item)=>(
-            <CharacterCard item = {item} key={item.id}/>
-         ))
-         }
-        </div>
-    )} 
-
-    const kf = keyframes`
-    100% {
-      opacity: 1;
-      transform: scale(1);
-  }
-`
-const CharacterCard = ({item}) => {
+    
+const Character = props => {
+    const {info} = props
     return (
-   <div className='card'>
-    <h3>{item.name}</h3>
-    <p> Gender: {item.gender}</p>
-    <p> Height: {item.height}</p>
-    <p> Mass: {item.mass}</p>
-    <p> Birth Year: {item.birth_year}</p>
-    <p> Eye Color: {item.eye_color}</p>
-    <p> Hair Color: {item.hair_color}</p>
-    <p> Mass: {item.mass}</p>
-    <p> Skin Color: {item.skin_color}</p>
-    </div>
-)}
+        <Style>
+            <p>{info.name} was born in {info.birth_year}. 
+            They're {info.height}km tall.</p>
+        </Style>
+    )
+}
+
+export default Character;
